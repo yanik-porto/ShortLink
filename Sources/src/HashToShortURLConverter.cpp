f90035eef5c0b5be6e7c93643f54e7373fc3cc62
yanik-porto <yanik.porto@gmail.com>
2021-04-16 15:31:02 +0200

    Implement HashToShortURL method

--- Sources/src/HashToShortURLConverter.cpp
+++ Sources/src/HashToShortURLConverter.cpp
@@ -1,5 +1,7 @@
 #include "include/private/HashToShortURLConverter.h"
 
+#include <algorithm>
+
 HashToShortURLConverter::HashToShortURLConverter() {
 
 }
@@ -9,7 +11,16 @@ HashToShortURLConverter::~HashToShortURLConverter() {
 }
 
 std::string HashToShortURLConverter::HashToShortURL(unsigned int hash) const {
+    char alphabet[] = "abcdefghijklmnopqrstuvwxyz0123456789";
 
+    std::string shortUrl;
+    while (hash) {
+        shortUrl.push_back(alphabet[hash % 36]);
+        hash /= 36;
+    }
+
+    std::reverse(shortUrl.begin(), shortUrl.end());
+    return shortUrl;
 }
 
 unsigned int HashToShortURLConverter::ShortURLToHash(const std::string &shortUrl) const {
